23ad6bbc37c6a81d0426f24dcc1d93c8529566b2
Jivanshu-Mittal <jivanshumitt1052@gmail.com>
2025-05-27 12:31:39 +0530

    Main File has been updated

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        /*Autoboxing 
+         * Autoboxing is the automatic conversion that the Java compiler makes between the primitive types and their corresponding object wrapper classes.
+         * For example, converting an int to an Integer, a double to a Double, and so on.
+         * Unboxing
+         * Unboxing is the reverse process, where the object wrapper class is converted back to its corresponding primitive type.
+         * For example, converting an Integer to an int, a Double to a double, and so on.
+         * wrapper class
+         * Wrapper classes in Java are used to convert primitive data types into objects.
+         * Each primitive type has a corresponding wrapper class:
+         * int -> Integer
+         * double -> Double         
+         * boolean -> Boolean
+         * char -> Character
+         *  byte -> Byte
+         * short -> Short
+         * long -> Long
+         * float -> Float
+         * These wrapper classes are part of the java.lang package and provide methods to convert between primitive types and their object representations.
+         */
     }
 }

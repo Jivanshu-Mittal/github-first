1f9f001d133c96ba6adcc51b4c12ba97e0166b4c
Jivanshu-Mittal <jivanshumitt1052@gmail.com>
2025-06-12 15:56:07 +0530

    New things learned

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        //5-1 using while loop
+        for(int i=5; i>0; i--){
+            System.out.println(i);
+        }
+        //1-10 using for loop
         for(int i=0; i<11; i++){
             System.out.println("For loop " + i);
         }
+        //even numbers using for loop
+        for(int i=0; i<21; i+=2){
+            System.out.println(i);
+        }
+        //even odd
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        System.out.println("check if number is even or odd. " + (n % 2 == 0 ? "Even" : "odd"));
     }
 }

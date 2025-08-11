76fb7ee9bd0e498961605b52af9c180b41820104
Jivanshu-Mittal <jivanshumitt1052@gmail.com>
2025-08-11 23:05:39 +0530

    for loop for printing 1 to 10 removed .

--- Main.java
+++ Main.java
@@ -10,10 +10,6 @@ public class Main {
         //5-1 using while loop
         for(int i=5; i>0; i--){
             System.out.println(i);
-        }
-        //1-10 using for loop
-        for(int i=0; i<11; i++){
-            System.out.println("For loop " + i);
-        }       
+        }     
     }
 }
